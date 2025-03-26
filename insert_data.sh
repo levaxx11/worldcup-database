6108a412afe8633235ea2238b415259e5e365afc
Lev Khodzhaev <levaxx11@gmail.com>
2025-03-26 09:22:58 +0000

    Add World Cup project files

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,27 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+# Clear existing data
+echo $($PSQL "TRUNCATE TABLE games, teams RESTART IDENTITY;")
+
+# Insert unique teams from games.csv
+cat games.csv | tail -n +2 | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  # Insert winner team if not exists
+  echo $($PSQL "INSERT INTO teams(name) VALUES('$WINNER') ON CONFLICT (name) DO NOTHING;")
+  # Insert opponent team if not exists
+  echo $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT') ON CONFLICT (name) DO NOTHING;")
+done
+
+# Insert game data
+cat games.csv | tail -n +2 | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  # Get winner_id
+  WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
+  # Get opponent_id
+  OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
+  # Insert game
+  echo $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) 
+                VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS);")
+done
